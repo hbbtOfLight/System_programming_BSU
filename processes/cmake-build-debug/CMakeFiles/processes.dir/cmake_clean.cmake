ea15b41c0dc16adf5c170b0e2d5cb837805f1489
hbbtOfLight <kataryna.kazinskaya@gmail.com>
2021-10-18 00:27:08 +0300

    Add processes lab

---
+++ processes/cmake-build-debug/CMakeFiles/processes.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/processes.dir/main.c.obj"
+  "libprocesses.dll.a"
+  "processes.exe"
+  "processes.exe.manifest"
+  "processes.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/processes.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
